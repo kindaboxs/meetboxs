af8fe784105792bb5390e1c303e04a5fd87158e7
MrBoxs <rizkywanda200155@gmail.com>
2025-05-28 17:20:07 +0700

    06: dashboard sidebar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+
+interface Props {
+	children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: Props) {
+	return (
+		<SidebarProvider>
+			<DashboardSidebar />
+			<SidebarInset>
+				<main className="bg-muted flex h-dvh w-full flex-col">{children}</main>
+			</SidebarInset>
+		</SidebarProvider>
+	);
+}
---
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+import { BotIcon, StarIcon, VideoIcon } from "lucide-react";
+
+import { BoxsIcon } from "@/components/icons/boxs-icon";
+import { Separator } from "@/components/ui/separator";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	useSidebar,
+} from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { authClient } from "@/lib/auth-client";
+import { cn } from "@/lib/utils";
+import { DashboardUserButton } from "@/modules/dashboard/ui/components/dashboard-user-button";
+
+const firstSection = [
+	{
+		label: "Meetings",
+		icon: VideoIcon,
+		href: "/meetings",
+	},
+	{
+		label: "Agents",
+		icon: BotIcon,
+		href: "/agents",
+	},
+];
+
+const secondSection = [
+	{
+		label: "Upgrade",
+		icon: StarIcon,
+		href: "/upgrade",
+	},
+];
+
+export const DashboardSidebar = () => {
+	const pathname = usePathname();
+	const router = useRouter();
+
+	const { data: session, isPending } = authClient.useSession();
+	const { isMobile } = useSidebar();
+
+	const handleSignOut = async () => {
+		await authClient.signOut({
+			fetchOptions: {
+				onSuccess: () => {
+					router.push("/sign-in");
+				},
+			},
+		});
+	};
+
+	return (
+		<Sidebar>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton
+							size="lg"
+							className="text-sidebar-accent-foreground cursor-pointer"
+							asChild
+						>
+							<Link href="/">
+								<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-transparent">
+									<BoxsIcon className="size-8" />
+								</div>
+								<span className="text-2xl font-bold">MeetBoxs</span>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<div className="px-4 py-2">
+				<Separator />
+			</div>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{firstSection.map((item, index) => (
+								<SidebarMenuItem key={index}>
+									<SidebarMenuButton
+										className={cn("", {
+											"bg-sidebar-primary text-sidebar-primary-foreground hover:bg-sidebar-primary/80 hover:text-sidebar-primary-foreground transition-all duration-300 ease-in-out":
+												pathname === item.href,
+										})}
+										// isActive={pathname === item.href}
+										tooltip={item.label}
+										asChild
+									>
+										<Link href={item.href}>
+											<item.icon />
+											<span className="text-sm font-medium tracking-tight">
+												{item.label}
+											</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+				<SidebarGroup>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{secondSection.map((item, index) => (
+								<SidebarMenuItem key={index}>
+									<SidebarMenuButton
+										className={cn("", {
+											"bg-sidebar-primary text-sidebar-primary-foreground hover:bg-sidebar-primary/80 hover:text-sidebar-primary-foreground transition-all duration-300 ease-in-out":
+												pathname === item.href,
+										})}
+										tooltip={item.label}
+										asChild
+									>
+										<Link href={item.href}>
+											<item.icon />
+											<span className="text-sm font-medium tracking-tight">
+												{item.label}
+											</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+			<SidebarFooter>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						{isPending ? (
+							<Skeleton className="h-12 w-full" />
+						) : session ? (
+							<DashboardUserButton
+								session={JSON.parse(JSON.stringify(session))}
+								isMobile={isMobile}
+								onSignOut={handleSignOut}
+							/>
+						) : null}
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
+		</Sidebar>
+	);
+};
